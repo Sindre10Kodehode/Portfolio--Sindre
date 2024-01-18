4b099afa5200c29a1109574f098c30b901d17e0e
Sindre10Kodehode <sindre10kodehode@gmail.com>
2024-01-18 10:43:38 +0100

    *

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
  
  return(
     <header className="fixed z-30 flex justify-around w-full h-28 font-Lato">
-    <img src="/src/assets/wavesOpacity.svg" className="absolute w-full pointer-events-none -z-10"/>
+    <img src="/wavesOpacity.svg" className="absolute w-full pointer-events-none -z-10"/>
     <nav className="absolute flex justify-between m-4 text-xl mt-7 w-80 right-64">
         <a href="#hero" className="">Home</a>
         <a href="#about" className="">About</a>
