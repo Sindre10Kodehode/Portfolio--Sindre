0cd8cfffa3418694151bdbe7590a601abe765154
Sindre10Kodehode <sindre10kodehode@gmail.com>
2024-01-19 13:52:48 +0100

    *

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,8 +1,11 @@
 export const ProjectCard = ({ href, title, desc, link }) => {
   return (
-    <div className="w-[500px] border-8 border-[#300449] h-[400px] rounded-[50px] flex-col bg-[#300449] relative item-center flex m-8 text-[#E2C4F6] text-center font-Poppins">
-      <div className="w-full h-[170px] rounded-t-[41px] bg-white">
-        <img src={href} className="w-full h-full rounded-t-[41px]"/>
+    <div className="w-[500px] border-8 border-[#300449] h-[400px] rounded-[50px] flex-col bg-[#300449] relative item-center flex m-8 text-[#E2C4F6] text-center overflow-hidden font-Poppins">
+      <div className="w-full h-[180px] bg-white">
+        <img
+          src={href}
+          className="object-cover object-top w-full h-full overflow-hidden"
+        />
       </div>
       <h1 className="p-2 text-3xl">{title}</h1>
       <p>{desc}</p>
@@ -11,7 +14,11 @@ export const ProjectCard = ({ href, title, desc, link }) => {
         className="absolute bottom-2 left-2 h-14 w-14"
         href={link}
       >
-        <img src="/Github.png" className="bg-gray-300 h-full w-full rounded-[50%] p-1" />
+        <img
+          src="/Github.png"
+          className="bg-gray-300 h-full w-full rounded-[50%] p-1"
+          title="Link til GitHub repo"
+        />
       </a>
     </div>
   );
@@ -21,22 +28,16 @@ export const ProjectCards = () => {
   return (
     <div className="flex flex-wrap p-10">
       <ProjectCard
-        href=""
+        href="/pokedeximg.png"
         title="Pokedex"
-        desc="Som ett gruppeprosjekt lagde vi en pokedex. Dette er en index av bilder, navn og litt annen informasjon av forskjellige pokemon hentet fra en API."
-        link=""
+        desc="Som ett gruppeprosjekt lagde vi en pokedex. Dette er en index av bilder, navn og typer av forskjellige pokemon hentet fra en API."
+        link="https://github.com/Saikuura/Pokedex/tree/main"
       />
       <ProjectCard
         href=""
         title="React demo"
         desc="For å vise mine ferdigheter i "
-        link=""
-      />
-            <ProjectCard
-        href=""
-        title=""
-        desc=""
-        link=""
+        link="https://github.com/Sindre10Kodehode/React_Routing"
       />
     </div>
   );
