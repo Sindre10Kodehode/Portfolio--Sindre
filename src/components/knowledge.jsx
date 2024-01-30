7ed602bd75c1dc8168994851ca4ebbae22d817a4
Sindre10Kodehode <sindre10kodehode@gmail.com>
2024-01-30 11:14:55 +0100

    improved projects

--- src/components/knowledge.jsx
+++ src/components/knowledge.jsx
@@ -1,16 +1,16 @@
 export const IconCard = ({src, name}) => {
 
   return (
-      <div className="flex justify-between pt-6 flex-col p-5 place-items-center bg-[#6d1261] m-2 rounded-3xl w-28 h-44 text-[#E2C4F6] font-Poppins transition hover:scale-105">
-          <img src={src}/>
-          <h1>{name}</h1>
+      <div className="flex md:justify-between md:pt-6 flex-col md:p-5 w-12 h-14 justify-center place-items-center bg-[#6d1261] md:m-2 m-1 rounded-3xl md:w-28 md:h-44 text-[#E2C4F6] font-Poppins transition hover:scale-105">
+          <img src={src} className="w-[80%]"/>
+          <h1 className="hidden md:block">{name}</h1>
       </div>
   )
 }
 
 export const Knowledge = () => {
   return (
-    <section className="flex justify-center place-items-center bg-[#160022] p-6 rounded-[50px]">
+    <section className="flex justify-center flex-wrap place-items-center bg-[#160022] md:p-6 p-4 rounded-[50px]">
       <IconCard src="/html-5.png" name="HTML" />
       <IconCard src="/css-3.png" name="CSS" />
       <IconCard src="/javascript.png" name="Javascript" />
--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -5,7 +5,7 @@ export const Projects = () => {
   return (
     <section
       id="projects"
-      className="flex flex-col justify-center w-full h-screen bg-[#300449] text-[#E2C4F6] overflow-hidden place-items-center"
+      className="flex flex-col justify-center w-full h-[70rem] md:h-screen bg-[#300449] text-[#E2C4F6] overflow-hidden place-items-center"
     >
       <h1 className="z-50 p-2 text-5xl">Prosjekter</h1>
     <ProjectCards/>
